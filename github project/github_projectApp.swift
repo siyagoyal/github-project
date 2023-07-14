3909ab200e6d12851c926c484a59a7c814c8bc76
Siya <siyagoyal629@gmail.com>
2023-07-14 10:17:32 -0400

    Initial Commit

---
+++ github project/github_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_projectApp.swift
+//  github project
+//
+//  Created by Siya Goyal on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct github_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
